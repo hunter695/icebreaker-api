4ee84037d1cba2e19185a5568891bdcf60c3671c
Nathan <dobobobado@gmail.com>
2017-04-09 22:29:43 -0400

    added connection test, replaced throws with expects

--- src/__test__/mongo.test.js
+++ src/__test__/mongo.test.js
@@ -1,13 +1,18 @@
-import { storeOne } from '../mongodb'
+import { sampleWildcard, storeOne } from '../mongodb'
 import config from '../config' // for Twitter API keys and MongoDB URL
 
 const mongodb = require('mongodb')
 
-it('Stored object text should be retrieved.', () => {
-  mongodb.MongoClient.connect(config.mongodb.url, async (err, db) => {
-    if(err) {
-      throw Error(err)
-    }
+it('Should be able to connect to MongoDB.', () => {
+  mongodb.MongoClient.connect(config.mongodb.url, async (connectErr, db) => {
+    expect(connectErr).toBeFalsy()
+    db.close()
+  })
+})
+
+it('Stored document text should be retrieved.', () => {
+  mongodb.MongoClient.connect(config.mongodb.url, async (connectErr, db) => {
+    expect(connectErr).toBeFalsy()
     let insertedId
     const content = {
       text: 'tasty meatloaf',
@@ -21,14 +26,37 @@ it('Stored object text should be retrieved.', () => {
       let cursor
       try {
         // find the inserted document by its returned ID.
-        cursor = await db.collection('test').findOne({_id: insertedId}, {})
-      } catch(findErr) {
-        throw Error(findErr)
+        cursor = await db.collection('test').findOne({ _id: insertedId }, {})
+      } catch (findErr) {
+        expect(findErr).toBeFalsy()
       }
       const text = cursor.text
       expect(text).toBe('tasty meatloaf')
-    } catch(storeErr) {
-      throw Error(storeErr)
+      db.collection('test').remove({ _id: insertedId })
+    } catch (storeErr) {
+      expect(storeErr).toBeFalsy()
+    }
+    db.close()
+  })
+})
+
+it('Randomly sampling collection should return truthy', () => {
+  mongodb.MongoClient.connect(config.mongodb.url, async (connectErr, db) => {
+    expect(connectErr).toBeFalsy()
+    let insertedId
+    const content = {
+      text: 'tasty meatloaf',
+      author: 'Gordon Ramsay',
+      likes: 2,
+      dislikes: 123,
+    }
+    try {
+      insertedId = await storeOne(db, 'test', content)
+      const sample = await sampleWildcard(db, 'test', 1)
+      expect(sample.text).toBeDefined()
+      db.collection('test').remove({ _id: insertedId })
+    } catch (storeErr) {
+      expect(storeErr).toBeFalsy()
     }
     db.close()
   })
