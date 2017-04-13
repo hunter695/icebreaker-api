24835e0f865841751b0e22ba89cd7b0f3a577bb5
Nathan <dobobobado@gmail.com>
2017-04-12 22:21:45 -0400

    add curated database test

--- src/__test__/mongo.test.js
+++ src/__test__/mongo.test.js
@@ -1,4 +1,4 @@
-import { sampleWildcard, storeOne } from '../mongodb'
+import { sampleWildcard, addLikes, storeObject, storeContribution } from '../mongodb'
 import config from '../config' // for Twitter API keys and MongoDB URL
 
 const mongodb = require('mongodb')
@@ -15,7 +15,7 @@ it('Stored document text should be retrieved.', () => {
     }
     try {
       // store into temporary collection 'test' the content.
-      insertedId = await storeOne(db, 'test', content)
+      insertedId = await storeObject(db, 'test', content)
       let cursor
       try {
         // find the inserted document by its returned ID.
@@ -33,7 +33,7 @@ it('Stored document text should be retrieved.', () => {
   })
 })
 
-it('Randomly sampling collection should return truthy', () => {
+it('Randomly sampling collection should return a document', () => {
   mongodb.MongoClient.connect(config.mongodb.url, async (connectErr, db) => {
     expect(db).toBeTruthy()
     let insertedId
@@ -44,12 +44,83 @@ it('Randomly sampling collection should return truthy', () => {
       dislikes: 123,
     }
     try {
-      insertedId = await storeOne(db, 'test', content)
+      insertedId = await storeObject(db, 'test', content)
       const sample = await sampleWildcard(db, 'test', 1)
       expect(sample.text).toBeTruthy()
       db.collection('test').remove({ _id: insertedId })
     } catch (storeErr) {
       expect(storeErr).toBeFalsy()
+    }
+    db.close()
+  })
+})
+
+it('storeContribution test', () => {
+  mongodb.MongoClient.connect(config.mongodb.url, async (connectErr, db) => {
+    expect(db).toBeTruthy()
+    let insertedId
+    const text = 'tasty meatloaf'
+    const author = 'Gordon Ramsay'
+    try {
+      insertedId = await storeContribution(db, 'test', text, author)
+      let cursor
+      try {
+        // find the inserted document by its returned ID.
+        cursor = await db.collection('test').findOne({ _id: insertedId }, {})
+      } catch (findErr) {
+        expect(findErr).toBeFalsy()
+      }
+      const retrievedText = cursor.text
+      expect(retrievedText).toBe('tasty meatloaf')
+      db.collection('test').remove({ _id: insertedId })
+    } catch (storeErr) {
+      expect(storeErr).toBeFalsy()
+    }
+    db.close()
+  })
+})
+
+it('curated csv database addLikes test', () => {
+  mongodb.MongoClient.connect(config.mongodb.url, async (connectErr, db) => {
+    expect(db).toBeTruthy()
+    let currentLikes
+    let cursor
+
+    // find a curated document for testing
+    try {
+      // find the inserted document by its returned ID.
+      cursor = await db.collection('icebreaker_curated').findOne()
+      currentLikes = cursor.likes
+    } catch (findErr) {
+      expect(findErr).toBeFalsy()
+    }
+    const objectId = cursor._id
+
+    // add 10 likes to the retrieved document
+    try {
+      await addLikes(db, 'icebreaker_curated', objectId, 10)
+
+    } catch (updateErr) {
+      expect(updateErr).toBeFalsy()
+    }
+
+    // find the document the test added 10 likes to.
+    try {
+      // find the inserted document by its returned ID.
+      cursor = await db.collection('icebreaker_curated').findOne({ _id: objectId }, {})
+    } catch (findErr) {
+      expect(findErr).toBeFalsy()
+    }
+
+    // verify that the difference is 10
+    expect(cursor.likes - currentLikes).toBe(10)
+    
+    // reset the value so nothing is changed at the end of this test
+    try {
+      // find the inserted document by its returned ID.
+      await db.collection('icebreaker_curated').update({ _id: objectId }, { $set: {likes:currentLikes}})
+    } catch (updateErr) {
+      expect(updateErr).toBeFalsy()
     }
     db.close()
   })
