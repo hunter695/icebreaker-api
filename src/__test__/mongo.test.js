1a4443fe6016db8135a21428a576722c439628ce
Nathan <dobobobado@gmail.com>
2017-04-14 01:02:21 -0400

    move tests out of mongodb connect() callback

--- src/__test__/mongo.test.js
+++ src/__test__/mongo.test.js
@@ -3,9 +3,11 @@ import config from '../config' // for Twitter API keys and MongoDB URL
 
 const mongodb = require('mongodb')
 
-it('Stored document text should be retrieved.', () => {
-  mongodb.MongoClient.connect(config.mongodb.url, async (connectErr, db) => {
+it('Stored document text should be retrieved.', async () => {
+  const db = await mongodb.MongoClient.connect(config.mongodb.url)
+  try {
     expect(db).toBeTruthy()
+
     let insertedId
     const content = {
       text: 'tasty meatloaf',
@@ -13,30 +15,27 @@ it('Stored document text should be retrieved.', () => {
       likes: 2,
       dislikes: 123,
     }
-    try {
-      // store into temporary collection 'test' the content.
-      insertedId = await storeObject(db, 'test', content)
-      let cursor
-      try {
-        // find the inserted document by its returned ID.
-        cursor = await db.collection('test').findOne({ _id: insertedId }, {})
-        expect(cursor).toBeTruthy()
-      } catch (findErr) {
-        expect(findErr).toBeFalsy()
-      }
-      const text = cursor.text
-      expect(text).toBe('tasty meatloaf')
-      db.collection('test').remove({ _id: insertedId })
-    } catch (storeErr) {
-      expect(storeErr).toBeFalsy()
-    }
+    // store into temporary collection 'test' the content.
+    insertedId = await storeObject(db, 'test', content)
+    let cursor
+    // find the inserted document by its returned ID.
+    cursor = await db.collection('test').findOne({ _id: insertedId }, {})
+    expect(cursor).toBeTruthy()
+
+    const text = cursor.text
+    expect(text).toBe('tasty meatloaf')
+    db.collection('test').remove({ _id: insertedId })
+
+  } finally {
     db.close()
-  })
+  }
 })
 
-it('Randomly sampling collection should return a document', () => {
-  mongodb.MongoClient.connect(config.mongodb.url, async (connectErr, db) => {
+it('Randomly sampling collection should return a document', async () => {
+  const db = await mongodb.MongoClient.connect(config.mongodb.url)
+  try {
     expect(db).toBeTruthy()
+
     let insertedId
     const content = {
       text: 'tasty meatloaf',
@@ -44,40 +43,35 @@ it('Randomly sampling collection should return a document', () => {
       likes: 2,
       dislikes: 123,
     }
-    try {
-      insertedId = await storeObject(db, 'test', content)
-      const sample = await sampleWildcard(db, 'test', 1)
-      expect(sample.text).toBeTruthy()
-      db.collection('test').remove({ _id: insertedId })
-    } catch (storeErr) {
-      expect(storeErr).toBeFalsy()
-    }
+
+    insertedId = await storeObject(db, 'test', content)
+    const sample = await sampleWildcard(db, 'test', 1)
+    expect(sample.text).toBeTruthy()
+    db.collection('test').remove({ _id: insertedId })
+
+  } finally {
     db.close()
-  })
+  }
 })
 
-it('storeContribution test', () => {
-  mongodb.MongoClient.connect(config.mongodb.url, async (connectErr, db) => {
+it('storeContribution test', async () => {
+  const db = await mongodb.MongoClient.connect(config.mongodb.url)
+  try {
     expect(db).toBeTruthy()
+
     let insertedId
     const text = 'tasty meatloaf'
     const author = 'Gordon Ramsay'
-    try {
-      insertedId = await storeContribution(db, 'test', text, author)
-      let cursor
-      try {
-        // find the inserted document by its returned ID.
-        cursor = await db.collection('test').findOne({ _id: insertedId }, {})
-        expect(cursor).toBeTruthy()
-      } catch (findErr) {
-        expect(findErr).toBeFalsy()
-      }
-      const retrievedText = cursor.text
-      expect(retrievedText).toBe('tasty meatloaf')
-      db.collection('test').remove({ _id: insertedId })
-    } catch (storeErr) {
-      expect(storeErr).toBeFalsy()
-    }
+    insertedId = await storeContribution(db, 'test', text, author)
+    let cursor
+    // find the inserted document by its returned ID.
+    cursor = await db.collection('test').findOne({ _id: insertedId }, {})
+    expect(cursor).toBeTruthy()
+    const retrievedText = cursor.text
+    expect(retrievedText).toBe('tasty meatloaf')
+    db.collection('test').remove({ _id: insertedId })
+
+  } finally {
     db.close()
-  })
+  }
 })
