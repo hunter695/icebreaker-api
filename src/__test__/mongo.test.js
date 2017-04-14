9d5b72ae56d501eb1f8feb2370bc0c997556e65d
Nathan <dobobobado@gmail.com>
2017-04-14 01:31:13 -0400

    change let to const and add await to mongodb remove()

--- src/__test__/mongo.test.js
+++ src/__test__/mongo.test.js
@@ -8,7 +8,6 @@ it('Stored document text should be retrieved.', async () => {
   try {
     expect(db).toBeTruthy()
 
-    let insertedId
     const content = {
       text: 'tasty meatloaf',
       author: 'Gordon Ramsay',
@@ -16,16 +15,14 @@ it('Stored document text should be retrieved.', async () => {
       dislikes: 123,
     }
     // store into temporary collection 'test' the content.
-    insertedId = await storeObject(db, 'test', content)
-    let cursor
+    const insertedId = await storeObject(db, 'test', content)
     // find the inserted document by its returned ID.
-    cursor = await db.collection('test').findOne({ _id: insertedId }, {})
+    const cursor = await db.collection('test').findOne({ _id: insertedId }, {})
     expect(cursor).toBeTruthy()
 
     const text = cursor.text
     expect(text).toBe('tasty meatloaf')
-    db.collection('test').remove({ _id: insertedId })
-
+    await db.collection('test').remove({ _id: insertedId })
   } finally {
     db.close()
   }
@@ -36,7 +33,6 @@ it('Randomly sampling collection should return a document', async () => {
   try {
     expect(db).toBeTruthy()
 
-    let insertedId
     const content = {
       text: 'tasty meatloaf',
       author: 'Gordon Ramsay',
@@ -44,11 +40,10 @@ it('Randomly sampling collection should return a document', async () => {
       dislikes: 123,
     }
 
-    insertedId = await storeObject(db, 'test', content)
+    const insertedId = await storeObject(db, 'test', content)
     const sample = await sampleWildcard(db, 'test', 1)
     expect(sample.text).toBeTruthy()
-    db.collection('test').remove({ _id: insertedId })
-
+    await db.collection('test').remove({ _id: insertedId })
   } finally {
     db.close()
   }
@@ -59,18 +54,15 @@ it('storeContribution test', async () => {
   try {
     expect(db).toBeTruthy()
 
-    let insertedId
     const text = 'tasty meatloaf'
     const author = 'Gordon Ramsay'
-    insertedId = await storeContribution(db, 'test', text, author)
-    let cursor
+    const insertedId = await storeContribution(db, 'test', text, author)
     // find the inserted document by its returned ID.
-    cursor = await db.collection('test').findOne({ _id: insertedId }, {})
+    const cursor = await db.collection('test').findOne({ _id: insertedId }, {})
     expect(cursor).toBeTruthy()
     const retrievedText = cursor.text
     expect(retrievedText).toBe('tasty meatloaf')
-    db.collection('test').remove({ _id: insertedId })
-
+    await db.collection('test').remove({ _id: insertedId })
   } finally {
     db.close()
   }
