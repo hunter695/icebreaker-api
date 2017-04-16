e353cc6d0bb8638453b727d40850df82ba4ca15b
Nathan <dobobobado@gmail.com>
2017-04-16 00:24:59 -0400

    update twitter API test to post, check, and delete

--- src/__test__/twitter.test.js
+++ src/__test__/twitter.test.js
@@ -1,8 +1,19 @@
 import { getWildcardFromTwitter } from '../twitter'
+import config from '../config' // for twitter API and MongoDB URL
 
-it('Should be able to fetch content from Twitter', () => {
-  const daysBack = 5
-  getWildcardFromTwitter('movies', 1, daysBack, (result) => {
-    expect(result.source).toBeTruthy()
+const twit = require('twit') // module for interacting with Twitter API
+const T = twit(config.twitter)
+
+it('Should be able to fetch content from Twitter', async () => {
+  const daysBack = 1
+  let idString
+  // tweet a message to make sure there is a tweet with #helloworld recently.
+  await T.post('statuses/update', { status: '#helloworld Twitter API Jest test' }, (err, data) => {
+    idString = data.id_str
   })
+  // get message from Twitter with #helloworld
+  let result = await getWildcardFromTwitter('helloworld', daysBack)
+  expect(result).toBeTruthy()
+  // delete the message
+  await T.post('statuses/destroy/:id', { id: idString })
 })
