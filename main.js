5359d5618220b31baf925698870201d0b75e3c64
Igor Goldvekht <goldvekht.igor@gmail.com>
2017-04-15 21:12:43 -0400

    Implement 'get icebreaker_curated' on server

--- main.js
+++ main.js
@@ -1,8 +1,26 @@
 import express from 'express'
+import mongodb from 'mongodb'
+import config from './src/config'
+import { getRandomDocument } from './src/mongodb'
 const app = express()
+
+
+const mongodbURL = config.mongodb.url
+const MongoClient = mongodb.MongoClient
+
+const ICEBREAKER_CURATED = 'icebreaker_curated'
+
+let db
 
 const PORT = process.env.PORT || 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
+app.get('/icebreaker', async (req, res) => {
+  res.send(await getRandomDocument(db, ICEBREAKER_CURATED))
+})
+
+MongoClient.connect(mongodbURL, (err, database) => {
+  db = database
+  app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
+})
