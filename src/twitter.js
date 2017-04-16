dc925e18167933c4325bc1e1fe87084ca6ac86cb
Nathan <dobobobado@gmail.com>
2017-04-16 00:24:21 -0400

    remove callback from getWildcardFromTwitter, now returns the result

--- src/twitter.js
+++ src/twitter.js
@@ -9,14 +9,12 @@ const T = twit(config.twitter)
 /**
  * Passes acquired wildcard tweet to callback
  * @param {string} content a string such as 'pickupline(s)' or 'icebreaker(s)'
+ * @param {number} amount how many tweets you want back.
  * @param {string} daysBack how far back in time to get tweets
- * @param {requestCallback} callback runs after successful Twitter API call,
  * with an object containing information from tweet passed to it. If the
  * retrieval fails, the null is passed to callback().
  */
-export async function getWildcardFromTwitter(content, daysBack, callback) {
-  let tweet
-  let result
+export async function getWildcardFromTwitter(content, daysBack) {
   let date
   try {
     date = await getPastDate(daysBack)
@@ -30,19 +28,22 @@ export async function getWildcardFromTwitter(content, daysBack, callback) {
     result_type: 'recent',
     count: 1,
   }
-  T.get('search/tweets', params, (twitterError, data, response) => {
-    tweet = data.statuses
-    if (response.statusCode !== 200 || (Array.isArray(tweet) && tweet.length === 0)) {
-      callback(null)
-    } else {
-      result = {
-        text: tweet[0].text,
-        author: tweet[0].user.screen_name,
-        retweet_count: tweet[0].retweet_count,
-        source: tweet[0].source,
-        created_at: tweet[0].created_at,
-      }
-      callback(result)
+  let twitterResult
+  try {
+    twitterResult = await T.get('search/tweets', params)
+  } catch (twitterError) {
+    throw Error(twitterError)
+  }
+  let result
+  const tweet = twitterResult.data.statuses
+  if (twitterResult.resp.statusCode === 200 || (Array.isArray(tweet) && tweet.length !== 0)) {
+    result = {
+      text: tweet[0].text,
+      author: tweet[0].user.screen_name,
+      retweet_count: tweet[0].retweet_count,
+      source: tweet[0].source,
+      created_at: tweet[0].created_at,
     }
-  })
+  }
+  return result
 }
