84853b24a1afb9dcea376453400658964abf8fb8
Igor Goldvekht <goldvekht.igor@gmail.com>
2017-04-18 00:11:36 -0400

    Enable CORS

--- main.js
+++ main.js
@@ -9,6 +9,11 @@ const app = express()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next()
+})
 
 const mongodbURL = config.mongodb.url
 const MongoClient = mongodb.MongoClient
