d86641a9473f599f61a4ff91385d2cb9becbece1
Nathan <dobobobado@gmail.com>
2017-04-20 14:16:41 -0400

    add more days to daysBack

--- src/__test__/twitter.test.js
+++ src/__test__/twitter.test.js
@@ -5,15 +5,18 @@ const twit = require('twit') // module for interacting with Twitter API
 const T = twit(config.twitter)
 
 it('Should be able to fetch content from Twitter', async () => {
-  const daysBack = 1
+  const daysBack = 10
   let idString
+  const time = new Date().getTime()
   // tweet a message to make sure there is a tweet with #helloworld recently.
-  await T.post('statuses/update', { status: '#helloworld Twitter API Jest test' }, (err, data) => {
+  await T.post('statuses/update', { status: `#helloworld Twitter API Jest test ${time}` }, (err, data) => {
     idString = data.id_str
   })
   // get message from Twitter with #helloworld
   let result = await getTweet('helloworld', daysBack)
   expect(result).toBeTruthy()
   // delete the message
-  await T.post('statuses/destroy/:id', { id: idString })
+  if (idString){
+    await T.post('statuses/destroy/:id', { id: idString })
+  }
 })
