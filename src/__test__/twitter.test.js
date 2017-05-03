6a057ee9353d8b524f4050ca1367a6e136985946
Nathan <dobobobado@gmail.com>
2017-05-02 22:18:38 -0400

    remove post in twitter test

--- src/__test__/twitter.test.js
+++ src/__test__/twitter.test.js
@@ -1,22 +1,9 @@
 import { getTweet } from '../twitter'
-import config from '../config' // for twitter API and MongoDB URL
-
-const twit = require('twit') // module for interacting with Twitter API
-const T = twit(config.twitter)
 
 it('Should be able to fetch content from Twitter', async () => {
   const daysBack = 10
-  let idString
-  const time = new Date().getTime()
-  // tweet a message to make sure there is a tweet with #helloworld recently.
-  await T.post('statuses/update', { status: `#helloworld Twitter API Jest test ${time}` }, (err, data) => {
-    idString = data.id_str
-  })
+
   // get message from Twitter with #helloworld
   let result = await getTweet('helloworld', daysBack)
   expect(result).toBeTruthy()
-  // delete the message
-  if (idString){
-    await T.post('statuses/destroy/:id', { id: idString })
-  }
 })
